23b370d37f1b68c05f95158b0d3c2b07606454d7
martin211 <mail4evgeniy@gmail.com>
2015-07-14 00:54:20 +0300

    Menu item changes

--- AIMPDotNet/UI/MenuItem/MenuItem.cs
+++ AIMPDotNet/UI/MenuItem/MenuItem.cs
@@ -5,6 +5,8 @@ namespace AIMP.SDK.UI.MenuItem
 {
     using System.Drawing;
 
+    internal delegate void SubMenuAdded(MenuItem item);
+
     /// <summary>
     /// Base menu item class.
     /// </summary>
@@ -18,6 +20,7 @@ namespace AIMP.SDK.UI.MenuItem
         protected MenuItem(string text) : base(text)
         {
             _visible = true;
+            ChildItems = new MenuItemCollection();
         }
 
         /// <summary>
@@ -102,6 +105,13 @@ namespace AIMP.SDK.UI.MenuItem
         public event EventHandler BeforeShow;
 
         public Bitmap Glyph { get; set; }
+
+        public void AddSubItem(MenuItem item)
+        {
+            ChildItems.Add(this, item);
+        }
+
+        public IntPtr AimpMenuItemHeader { get; set; }
 
         /// <summary>
         /// Called when [click].
