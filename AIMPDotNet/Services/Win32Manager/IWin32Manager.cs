cc4374e5971549509adcd41c4e984996178b63cb
martin211 <mail4evgeniy@gmail.com>
2015-10-23 00:54:36 +0300

    Implement WIN32 functions

--- AIMPDotNet/Services/Win32Manager/IWin32Manager.cs
+++ AIMPDotNet/Services/Win32Manager/IWin32Manager.cs
@@ -1,9 +1,14 @@
 ﻿namespace AIMP.SDK.Services.Win32Manager
 {
     using System;
+    using System.Drawing;
 
     public interface IWin32Manager
     {
+        IntPtr GetAimpHandle();
+
         void SetParentWindow(IntPtr child, IntPtr parent);
+
+        Point GetWindowPosition(IntPtr handle);
     }
 }
