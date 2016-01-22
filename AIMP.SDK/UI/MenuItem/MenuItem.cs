4d38e193f201ee5bca94d9369e85186b41bae5dd
martin211 <mail4evgeniy@gmail.com>
2016-01-22 08:35:17 +0300

    Changed MemuManager

--- AIMP.SDK/UI/MenuItem/MenuItem.cs
+++ AIMP.SDK/UI/MenuItem/MenuItem.cs
@@ -3,44 +3,27 @@ namespace AIMP.SDK.UI.MenuItem
 {
     using System.Drawing;
 
+    using AIMP.SDK.ActionManager;
     using AIMP.SDK.MenuManager;
-
-    internal delegate void SubMenuAdded(MenuItem item);
 
     /// <summary>
     /// Base menu item class.
     /// </summary>
     // ReSharper disable once InconsistentNaming
-    public abstract class MenuItem : AimpUIItem
+    public abstract class MenuItem : IAimpMenuItem
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MenuItem"/> class.
         /// </summary>
         /// <param name="text">The text.</param>
-        protected MenuItem(string text) : base(text)
+        protected MenuItem()
         {
-            _visible = true;
-            _enabled = true;
-            ChildItems = new MenuItemCollection();
+            Visible = true;
+            Enabled = true;
+            Id = $"aimp_uiitem_{GetHashCode()}";
         }
 
-        /// <summary>
-        /// Gets or sets the parent menu identifier.
-        /// </summary>
-        /// <value>
-        /// The parent menu identifier.
-        /// </value>
-        public ParentMenuType ParentMenuId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the type of the menu item.
-        /// </summary>
-        /// <value>
-        /// The type of the menu item.
-        /// </value>
-        public MenuItemType MenuItemType { get; protected set; }
-        
-        private bool _visible;
+        public AimpMenuItemStyle Style { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether this <see cref="MenuItem"/> is visible.
@@ -48,116 +31,38 @@ namespace AIMP.SDK.UI.MenuItem
         /// <value>
         ///   <c>true</c> if visible; otherwise, <c>false</c>.
         /// </value>
-        public bool Visible 
-        {
-            get
-            {
-                return _visible;
-            }
-            set
-            {
-                if (_visible != value)
-                {
-                    _visible = value;
-                    OnPropertyChanged("Visible");
-                }
-            }
-        }
+        public bool Visible { get; set; }
 
         /// <summary>
-        /// Gets or sets the child menu items.
+        /// Deletes the children.
         /// </summary>
-        /// <value>
-        /// The child items.
-        /// </value>
-        public MenuItemCollection ChildItems { get; set; }
-
-        private AimpUIItem _actionItem;
-
-        /// <summary>
-        /// Gets or sets the associated action item.
-        /// </summary>
-        public AimpUIItem ActionItem
+        public AimpActionResult DeleteChildren()
         {
-            get
-            {
-                return _actionItem;
-            }
-            set
-            {
-                if (_actionItem == null || !_actionItem.Equals(value))
-                {
-                    _actionItem = value;
-                    OnPropertyChanged("ActionItem");
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public Bitmap Image { get; set; }
 
         private bool _enabled;
 
-        public bool Enabled
-        {
-            get
-            {
-                return _enabled;
-            }
-            set
-            {
-                _enabled = value;
-                OnPropertyChanged("Enabled");
-            }
-        }
+        public string Custom { get; set; }
 
-        /// <summary>
-        /// Occurs when button was clicked.
-        /// </summary>
-        public event EventHandler Click;
+        public IAimpAction Action { get; set; }
 
-        /// <summary>
-        /// Occurs before button will be show.
-        /// </summary>
-        public event EventHandler BeforeShow;
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Checked { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public event EventHandler OnExecute;
+
+        public event EventHandler OnShow;
 
         public Bitmap Glyph { get; set; }
 
-        public void AddSubItem(MenuItem item)
-        {
-            ChildItems.Add(this, item);
-        }
-
-        public void InsertSubItem(MenuItem item, int index)
-        {
-            if (index < 0 || index > ChildItems.Count)
-            {
-                throw new IndexOutOfRangeException();
-            }
-            
-            ChildItems.InsertItem(index, item);
-        }
-
-        public IntPtr AimpMenuItemHeader { get; set; }
-
-        /// <summary>
-        /// Called when [click].
-        /// </summary>
-        public virtual void OnClick()
-        {
-            var tmp = System.Threading.Interlocked.CompareExchange(ref Click, null, null);
-            if (tmp != null)
-            {
-                tmp(this, EventArgs.Empty);
-            }
-        }
-
-        public virtual void OnBeforeShow()
-        {
-            var tmp = System.Threading.Interlocked.CompareExchange(ref BeforeShow, null, null);
-            if (tmp != null)
-            {
-                tmp(this, EventArgs.Empty);
-            }            
-        }
+        public IAimpMenuItem Parent { get; set; }
     }
 }
