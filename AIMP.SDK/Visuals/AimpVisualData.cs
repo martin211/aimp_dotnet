e8727a1727e34ea1596dad0db559fe394973f79a
martin211 <mail4evgeniy@gmail.com>
2016-01-26 17:36:03 +0300

    Added definitions for the Visuals service

---
+++ AIMP.SDK/Visuals/AimpVisualData.cs
@@ -0,0 +1,11 @@
+﻿namespace AIMP.SDK.Visuals
+{
+    public class AimpVisualData
+    {
+        public byte[] Peaks { get; set; }
+
+        public byte[] Spectrum { get; set; }
+
+        public byte[] WaveForm { get; set; }
+    }
+}
---
+++ AIMP.SDK/Visuals/IAimpExtensionCustomVisualization.cs
@@ -0,0 +1,9 @@
+﻿namespace AIMP.SDK.Visuals
+{
+    public interface IAimpExtensionCustomVisualization
+    {
+        AimpVisualFlags GetFlags();
+
+        void Draw(AimpVisualData data);
+    }
+}
