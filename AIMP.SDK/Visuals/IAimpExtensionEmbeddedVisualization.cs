ff6007539f558c1726d9bbe1531f0cca24a20e90
Evgeniy <mail4evgeniy@gmail.com>
2016-01-26 17:36:03 +0300

    Added definitions for the Visuals service

---
+++ AIMP.SDK/Visuals/IAimpExtensionEmbeddedVisualization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AIMP.SDK.Visuals
+{
+    public enum AimpVisualFlags
+    {
+        AIMP_VISUAL_FLAGS_RQD_DATA_WAVE,
+        AIMP_VISUAL_FLAGS_RQD_DATA_SPECTRUM,
+        AIMP_VISUAL_FLAGS_NOT_SUSPEND
+    }
+
+    public interface IAimpExtensionEmbeddedVisualization
+    {
+        event EventHandler Click;
+
+        AimpVisualFlags GetFlags();
+
+        AimpActionResult GetMaxDisplaySize(out int width, out int height);
+
+        AimpActionResult GetName(out string name);
+
+        AimpActionResult Initialize(int width, int height);
+
+        void Finalize();
+
+        void Draw(IntPtr dc, AimpVisualData data);
+
+        void Resize(int width, int height);
+    }
+}
