eaca24dc5d2199e32293177052cac52552ba572b
martin211 <mail4evgeniy@gmail.com>
2016-02-12 17:03:35 +0300

    Extract Play List Listner

---
+++ AIMP.SDK/PlayList/IAimpPlaylistListener.cs
@@ -0,0 +1,15 @@
+﻿namespace AIMP.SDK.PlayList
+{
+    public delegate void PlayListChangedHandler(IAimpPlayList sender, PlayListNotifyType notifType);
+
+    public delegate void AimpPlayListHandler(IAimpPlayList sender);
+
+    public interface IAimpPlaylistListener
+    {
+        event AimpPlayListHandler Activated;
+
+        event AimpPlayListHandler Removed;
+
+        event PlayListChangedHandler Changed;
+    }
+}
