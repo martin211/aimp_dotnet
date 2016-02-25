24bb85ce3615139013e764031835db415fa75680
martin211 <mail4evgeniy@gmail.com>
2016-02-26 00:58:55 +0300

    Updated demo plugin

--- Plugins/Demo/PlayListControl.Designer.cs
+++ Plugins/Demo/PlayListControl.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // listView1
@@ -37,24 +36,15 @@
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(461, 366);
+            this.listView1.Size = new System.Drawing.Size(461, 410);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 366);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(461, 44);
-            this.panel1.TabIndex = 1;
             // 
             // PlayListControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.panel1);
             this.Name = "PlayListControl";
             this.Size = new System.Drawing.Size(461, 410);
             this.ResumeLayout(false);
@@ -64,6 +54,5 @@
         #endregion
 
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Panel panel1;
     }
 }
