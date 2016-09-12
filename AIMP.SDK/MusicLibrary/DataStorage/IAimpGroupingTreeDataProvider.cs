b4e6e856800aa120674e373af6e99063f0b1c789
Evgeniy <mail4evgeniy@gmail.com>
2016-09-12 17:46:21 +0300

    Add interfaces for Grouping Tree Data Provider

--- AIMP.SDK/MusicLibrary/DataStorage/IAimpGroupingTreeDataProvider.cs
+++ AIMP.SDK/MusicLibrary/DataStorage/IAimpGroupingTreeDataProvider.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using AIMP.SDK.MusicLibrary.DataFilter;
 
 namespace AIMP.SDK.MusicLibrary.DataStorage
@@ -12,14 +11,37 @@ namespace AIMP.SDK.MusicLibrary.DataStorage
         AIMPML_GROUPINGTREEDATAPROVIDER_CAP_DONTSORT
     }
 
+    /// <summary>
+    /// Interface provides an ability to specify custom data provider for a grouping tree.
+    /// </summary>
     public interface IAimpGroupingTreeDataProvider
     {
+        /// <summary>
+        /// Plugin must build the data filter for table based on current Selection. 
+        /// </summary>
+        /// <param name="filter">The filter.</param>
+        /// <param name="selection">The selection.</param>
         AimpActionResult AppendFilter(IAimpDataFilterGroup filter, IAimpGroupingTreeSelection selection);
 
+        /// <summary>
+        /// Gets the capabilities.
+        /// </summary>
+        /// <returns></returns>
         CapabilitiesFlags GetCapabilities();
 
+        /// <summary>
+        /// Returns the data for next level of selected node in grouping tree.
+        /// </summary>
+        /// <param name="selection">The selection.</param>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
         AimpActionResult GetData(IAimpGroupingTreeSelection selection, out IAimpGroupingTreeDataProviderSelection data);
 
+        /// <summary>
+        /// Returns the name of top level field for alphabetic index.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns></returns>
         AimpActionResult GetFieldForAlphabeticIndex(string fileName);
     }
 }
