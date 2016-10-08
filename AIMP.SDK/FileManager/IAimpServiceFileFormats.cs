0deda27e1762e483f6c27b2d43a8669ffd9ee9a1
Evgeniy <mail4evgeniy@gmail.com>
2016-10-08 22:36:18 +0300

    Remove unused files Fix Initialize all the members of this enumeration error

--- AIMP.SDK/FileManager/IAimpServiceFileFormats.cs
+++ AIMP.SDK/FileManager/IAimpServiceFileFormats.cs
@@ -5,8 +5,8 @@ namespace AIMP.SDK.FileManager
     [Flags]
     public enum FileFormats
     {
-        AIMP_SERVICE_FILEFORMATS_CATEGORY_AUDIO,
-        AIMP_SERVICE_FILEFORMATS_CATEGORY_PLAYLISTS
+        AIMP_SERVICE_FILEFORMATS_CATEGORY_AUDIO = 1,
+        AIMP_SERVICE_FILEFORMATS_CATEGORY_PLAYLISTS = 2
     }
 
     public interface IAimpServiceFileFormats
