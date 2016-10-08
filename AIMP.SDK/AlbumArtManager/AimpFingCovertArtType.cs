5af1ab5d7713f1ddd2d0d203e8b0528f0ae7e9a6
martin211 <mail4evgeniy@gmail.com>
2016-10-08 22:36:18 +0300

    Remove unused files Fix Initialize all the members of this enumeration error

--- AIMP.SDK/AlbumArtManager/AimpFingCovertArtType.cs
+++ AIMP.SDK/AlbumArtManager/AimpFingCovertArtType.cs
@@ -8,7 +8,7 @@
     [Flags]
     public enum AimpFingCovertArtType
     {
-        None,
+        None = 0,
         AIMP_SERVICE_ALBUMART_FLAGS_IGNORECACHE = 1,
         AIMP_SERVICE_ALBUMART_FLAGS_ORIGINAL = 2,
         AIMP_SERVICE_ALBUMART_FLAGS_WAITFOR = 4
