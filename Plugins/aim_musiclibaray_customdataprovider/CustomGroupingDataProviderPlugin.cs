51ac50789b6cfb0c1587d2264302dc3a32d0410b
martin211 <mail4evgeniy@gmail.com>
2016-10-20 23:00:38 +0300

    Add new demo plugin for ML custom grouping provider

---
+++ Plugins/aim_musiclibaray_customdataprovider/CustomGroupingDataProviderPlugin.cs
@@ -0,0 +1,16 @@
+﻿using AIMP.SDK;
+
+namespace aim_musiclibaray_customdataprovider
+{
+    [AimpPlugin("CustomGroupingDataProviderPlugin", "Martin", "1.0.0", AimpPluginType = AimpPluginType.Addons, FullDescription = "MusicLibrary CustomGroupingProvider demo plugin")]
+    public class CustomGroupingDataProviderPlugin : AimpPlugin
+    {
+        public override void Initialize()
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
