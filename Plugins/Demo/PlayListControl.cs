01601e1754d278077debdc2733c19f7ef2aa3c4b
Evgeniy <mail4evgeniy@gmail.com>
2016-12-08 23:33:11 +0300

    Fix build demo plugins

--- Plugins/Demo/PlayListControl.cs
+++ Plugins/Demo/PlayListControl.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using AIMP.SDK.PlayList;
 
 namespace DemoPlugin
@@ -53,9 +45,6 @@ namespace DemoPlugin
                 trackItem.Tag = item;
 
                 listView1.Items.Add(trackItem);
-
-                // free track resources.
-                item.Release();
             }
         }
     }
