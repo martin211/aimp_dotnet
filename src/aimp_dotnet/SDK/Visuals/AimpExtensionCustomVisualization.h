8f82e9c5bbe3e0a329ca05b1922643b509f548bd
Evgeniy <mail4evgeniy@gmail.com>
2017-01-26 23:30:39 +0300

    ADN-2 Extract code from headers files. Visuals

--- src/aimp_dotnet/SDK/Visuals/AimpExtensionCustomVisualization.h
+++ src/aimp_dotnet/SDK/Visuals/AimpExtensionCustomVisualization.h
@@ -2,7 +2,6 @@
 
 using namespace AIMP::SDK;
 
-
 class AimpExtensionCustomVisualization : public IUnknownInterfaceImpl<IAIMPExtensionCustomVisualization>
 {
 private:
@@ -11,40 +10,15 @@ private:
 public:
     typedef IUnknownInterfaceImpl<IAIMPExtensionCustomVisualization> Base;
 
-    AimpExtensionCustomVisualization(IAIMPCore *aimpCore, gcroot<AIMP::SDK::Visuals::IAimpExtensionCustomVisualization^> instance)
-    {
-        _managedObject = instance;
-        _aimpCore = aimpCore;
-    }
+    AimpExtensionCustomVisualization(IAIMPCore *aimpCore, gcroot<AIMP::SDK::Visuals::IAimpExtensionCustomVisualization^> instance);
 
     virtual int WINAPI GetFlags();
 
     virtual void WINAPI Draw(PAIMPVisualData Data);
 
-    virtual HRESULT WINAPI QueryInterface(REFIID riid, LPVOID* ppvObject)
-    {
-        if (!ppvObject)
-        {
-            return E_POINTER;
-        }
+    virtual HRESULT WINAPI QueryInterface(REFIID riid, LPVOID* ppvObject);
 
-        if (riid == IID_IAIMPExtensionCustomVisualization) {
-            *ppvObject = this;
-            AddRef();
-            return S_OK;
-        }
+    virtual ULONG WINAPI AddRef(void);
 
-        return E_NOINTERFACE;
-    }
-
-    virtual ULONG WINAPI AddRef(void)
-    {
-        return Base::AddRef();
-    }
-
-    virtual ULONG WINAPI Release(void)
-    {
-        return Base::Release();
-    }
+    virtual ULONG WINAPI Release(void);
 };
-
---
+++ src/aimp_dotnet/SDK/Visuals/AimpServiceVisualizations.cpp
@@ -0,0 +1,27 @@
+#include "Stdafx.h"
+#include "AimpServiceVisualizations.h"
+
+
+HRESULT WINAPI AimpServiceVisualizations::QueryInterface(REFIID riid, LPVOID* ppvObject)
+{
+    HRESULT res = Base::QueryInterface(riid, ppvObject);
+
+    if (riid == IID_IAIMPExtensionEmbeddedVisualization) {
+        *ppvObject = this;
+        AddRef();
+        return S_OK;
+    }
+
+    ppvObject = NULL;
+    return res;
+}
+
+ULONG WINAPI AimpServiceVisualizations::AddRef(void)
+{
+    return Base::AddRef();
+}
+
+ULONG WINAPI AimpServiceVisualizations::Release(void)
+{
+    return Base::Release();
+}
