d85f094d33eace77078f25d05d395a597526c8ad
Evgeniy <mail4evgeniy@gmail.com>
2017-01-27 00:10:00 +0300

    ADN-2 Extract code from headers files

---
+++ src/aimp_dotnet/SDK/PlayList/AimpExtensionPlaylistManagerListener.cpp
@@ -0,0 +1,47 @@
+#include "Stdafx.h"
+#include "AimpExtensionPlaylistManagerListener.h"
+
+
+AimpExtensionPlaylistManagerListener::AimpExtensionPlaylistManagerListener(gcroot<IAimpExtensionPlaylistManagerListenerExecutor^> instance)
+{
+    _managedInstance = instance;
+}
+
+void WINAPI AimpExtensionPlaylistManagerListener::PlaylistActivated(IAIMPPlaylist* Playlist)
+{
+    _managedInstance->OnPlaylistActivated(Playlist);
+}
+
+void WINAPI AimpExtensionPlaylistManagerListener::PlaylistAdded(IAIMPPlaylist* Playlist)
+{
+    _managedInstance->OnPlaylistAdded(Playlist);
+}
+
+void WINAPI AimpExtensionPlaylistManagerListener::PlaylistRemoved(IAIMPPlaylist* Playlist)
+{
+    _managedInstance->OnPlaylistRemoved(Playlist);
+}
+
+HRESULT WINAPI AimpExtensionPlaylistManagerListener::QueryInterface(REFIID riid, LPVOID* ppvObject)
+{
+    HRESULT res = Base::QueryInterface(riid, ppvObject);
+
+    if (riid == IID_IAIMPExtensionPlaylistManagerListener) {
+        *ppvObject = this;
+        AddRef();
+        return S_OK;
+    }
+
+    ppvObject = NULL;
+    return res;
+}
+
+ULONG WINAPI AimpExtensionPlaylistManagerListener::AddRef(void)
+{
+    return Base::AddRef();
+}
+
+ULONG WINAPI AimpExtensionPlaylistManagerListener::Release(void)
+{
+    return Base::Release();
+}
---
+++ src/aimp_dotnet/SDK/Win32/Win32Manager.cpp
@@ -0,0 +1,28 @@
+#include "Stdafx.h"
+#include "Win32Manager.h"
+
+using namespace System;
+
+void AIMP::Win32::Win32Manager::SetParentWindow(IntPtr child, IntPtr parent)
+{
+    SetParent((HWND)child.ToPointer(), (HWND)parent.ToPointer());
+}
+
+System::Drawing::Point AIMP::Win32::Win32Manager::GetWindowPosition(IntPtr handle)
+{
+    RECT rc;
+    HWND hndl = FindWindow("TAIMPMainForm", 0);
+
+    if (GetWindowRect(hndl, &rc))
+    {
+        return System::Drawing::Point(rc.left, rc.top);
+    }
+
+    return  System::Drawing::Point::Empty;
+}
+
+System::IntPtr AIMP::Win32::Win32Manager::GetAimpHandle()
+{
+    HWND hndl = FindWindow("TAIMPMainForm", 0);
+    return IntPtr(hndl);
+}
