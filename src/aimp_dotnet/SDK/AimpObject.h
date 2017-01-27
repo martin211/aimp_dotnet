b9f95b56293aee24babbdc74998f2c07655a3cc8
Evgeniy <mail4evgeniy@gmail.com>
2017-01-27 23:51:24 +0300

    ADN-19 Add header for each h file

--- src/aimp_dotnet/SDK/AimpObject.h
+++ src/aimp_dotnet/SDK/AimpObject.h
@@ -1,4 +1,10 @@
-﻿#pragma once
+/* AIMP DotNet SDK
+ * 
+ * (C) 2017
+ * Mail: mail4evgeniy@gmail.com
+ * https://github.com/martin211/aimp_dotnet
+ */
+#pragma once
 
 template<class TAimpObject>
 public interface class IAimpObject
