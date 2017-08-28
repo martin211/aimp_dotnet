a3f549181d44b21b931c41474c5049a92465fc42
Evgeniy <mail4evgeniy@gmail.com>
2017-08-28 22:31:24 +0300

    ADN-43 Fix return value at AimpDataStorageManager

--- src/aimp_dotnet/SDK/MusicLibrary/AimpDataStorageManager.h
+++ src/aimp_dotnet/SDK/MusicLibrary/AimpDataStorageManager.h
@@ -17,16 +17,16 @@ namespace AIMP
     {
         using namespace AIMP::SDK::MusicLibrary;
 
-        public ref class AimpDataStorageManager : 
+        public ref class AimpDataStorageManager :
             public AimpObject<IAIMPMLDataStorageManager>,
             public IAimpDataStorageManager
         {
         public:
             explicit AimpDataStorageManager(IAIMPMLDataStorageManager *manager);
 
-            virtual void BackgroundTaskStarted(int id, System::String ^cartion, AIMP::SDK::ActionManager::IAimpActionEvent ^cancelEvent);
+            virtual AimpActionResult BackgroundTaskStarted(int id, System::String ^cartion, AIMP::SDK::ActionManager::IAimpActionEvent ^cancelEvent);
 
-            virtual void BackgroundTaskFinished(int id);
+            virtual AimpActionResult BackgroundTaskFinished(int id);
 
             virtual void Changed();
         };
