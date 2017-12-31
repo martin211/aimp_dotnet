e098c617988e0854e1c794ec15ddf0ac1b02c0f2
Evgeniy <mail4evgeniy@gmail.com>
2017-12-31 16:07:11 +0300

    Fix build

---
+++ src/Plugins/dotnet_CustomFileSystem/CustomFileSystemPlugin.cs
@@ -0,0 +1,16 @@
+﻿namespace AIMP.SDK.CustomFileSystem
+{
+    [AimpPlugin("dotnet_CustomFileSystem", "AIMP DOTNET", "1", AimpPluginType = AimpPluginType.Addons)]
+    public class CustomFileSystemPlugin : AimpPlugin
+    {
+        public override void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
