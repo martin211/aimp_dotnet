d6c5a58573c29b5130cf72cf67d00b6697cb12d4
Evgeniy <mail4evgeniy@gmail.com>
2018-03-02 18:21:54 +0300

    [WIP] Add Nuke integration

--- build/Build.cs
+++ build/Build.cs
@@ -5,6 +5,7 @@ using Nuke.Common.Git;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.MSBuild;
 using Nuke.Core;
+using Nuke.Core.Tooling;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 using static Nuke.Core.IO.FileSystemTasks;
 using static Nuke.Core.IO.PathConstruction;
@@ -13,7 +14,7 @@ using static Nuke.Core.EnvironmentInfo;
 class Build : NukeBuild
 {
     // Console application entry. Also defines the default target.
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Version);
 
     // Auto-injection fields:
 
@@ -42,9 +43,20 @@ class Build : NukeBuild
             });
 
     Target Compile => _ => _
-            .DependsOn(Restore)
+            // .DependsOn(Restore)
             .Executes(() =>
             {
-                MSBuild(s => DefaultMSBuildCompile);
+                MSBuild(s => DefaultMSBuildCompile.SetNodeReuse(false));
             });
+
+    Target Version => _ => _
+        .Executes(() =>
+        {
+            var properties = GlobDirectories(SourceDirectory, "**/Properties");
+
+            foreach (var property in properties)
+            {
+                ProcessTasks.StartProcess(GitVersionTasks.DefaultGitVersion.ToolPath, $"/updateassemblyinfo {property}/AssemblyInfo.cs");
+            }
+        });
 }
