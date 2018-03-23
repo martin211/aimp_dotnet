fc52ad1096b21c9bbdca66e4aeb154443d0e9a42
Evgeniy <mail4evgeniy@gmail.com>
2018-03-23 11:08:30 +0300

    Add integration tests

---
+++ src/IntegrationTests/IntegrationFramework/BaseIntegrationTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationFramework
+{
+    public abstract class BaseIntegrationTest
+    {
+        private string _aimpPath;
+
+    }
+}
---
+++ src/IntegrationTests/IntegrationFramework/Core/IntegrationTestContext.cs
@@ -0,0 +1,11 @@
+﻿namespace IntegrationFramework.Core
+{
+    public class IntegrationTestContext
+    {
+        public string AimpVersion { get; set; }
+
+        public string AimpFolder { get; set; }
+
+        public string AimpProfileFolder { get; set; }
+    }
+}
