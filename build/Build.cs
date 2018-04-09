ce04b6015ba2fdf1b4f606a7370c951e8faa5b56
Evgeniy <mail4evgeniy@gmail.com>
2018-04-09 22:52:24 +0300

    CI intagration

--- build/Build.cs
+++ build/Build.cs
@@ -23,12 +23,18 @@ class Build : NukeBuild
     [GitVersion(DisableOnUnix = true)] readonly GitVersion GitVersion;
     [GitRepository(Branch = "master")] readonly GitRepository GitRepository;
 
+    [Parameter] readonly string SonarUrl;
+    [Parameter] readonly string SonarUser;
+    [Parameter] readonly string SonarPassword;
+    [Parameter] readonly string SonarProjectKey;
+    [Parameter] readonly string SonarProjectName;
+
     private string Source => NuGet
         ? "https://api.nuget.org/v3/index.json"
         : "https://www.myget.org/F/aimpsdk/api/v2/package";
 
     // Console application entry. Also defines the default target.
-    public static int Main () => Execute<Build>(x => x.SonarQube);
+    public static int Main () => Execute<Build>(x => x.Artifacts);
 
     Target Clean => _ => _
             .Executes(() =>
@@ -94,21 +100,9 @@ class Build : NukeBuild
         .Requires(() => ApiKey)
         .Requires(() => !NuGet || GitVersionAttribute.Bump.HasValue)
         .Requires(() => !NuGet || Configuration.EqualsOrdinalIgnoreCase("release"))
-        .Requires(() => !NuGet || GitVersion.BranchName.Equals("master"))
         .Executes(() =>
         {
             Logger.Info("Deploying Nuget packages");
-
-            //var packages = Directory.GetFiles(OutputDirectory, "AimpSDK.*");
-            //foreach (var package in packages)
-            //{
-            //    NuGetTasks.NuGetPush(c => c
-            //        .SetTargetPath(package)
-            //        .SetApiKey(ApiKey)
-            //        .SetSymbolApiKey(ApiKey)
-            //        .SetSource("https://www.myget.org/F/aimpsdk/api/v2/package")
-            //        .SetSymbolSource("https://www.myget.org/F/aimpsdk/symbols/api/v2/package"));
-            //}
             GlobFiles(OutputDirectory, "*.nupkg").NotEmpty()
                 .Where(c => !c.EndsWith("symbols.nupkg"))
                 .ForEach(c => NuGetTasks.NuGetPush(s => s
@@ -131,16 +125,17 @@ class Build : NukeBuild
 
     Target SonarQube => _ => _
         .DependsOn(Restore)
+        .Requires(() => SonarUrl, () => SonarUser)
         .Executes(() =>
         {
             SonarQubeTasks.SonarQubeBegin(c => c
                 .SetProjectBaseDir(SourceDirectory)
                 .SetWorkingDirectory(SourceDirectory)
-                .SetHostUrl("http://localhost:9000")
-                .SetProjectKey("aimp.test")
-                .SetProjectName("test")
-                .SetUserName("admin")
-                .SetUserPassword("admin")
+                .SetHostUrl(SonarUrl)
+                .SetProjectKey(SonarProjectKey)
+                .SetProjectName(SonarProjectName)
+                .SetUserName(SonarUser)
+                .SetUserPassword(SonarPassword)
                 .EnableVerbose(), new ProcessSettings());
         }, () =>
         {
@@ -148,16 +143,59 @@ class Build : NukeBuild
         }, () =>
         {
             SonarQubeTasks.SonarQubeEnd(c => c
-                .SetUserName("admin")
-                .SetUserPassword("admin")
+                .SetUserName(SonarUser)
+                .SetUserPassword(SonarPassword)
                 .SetWorkingDirectory(SourceDirectory)
             );
         });
 
     Target Artifacts => _ => _
-        .DependsOn(Compile)
+        //.DependsOn(Compile)
         .Executes(() =>
         {
-            var directories = GlobDirectories(SourceDirectory, $"**/bin/{Configuration}/");
+            EnsureCleanDirectory(OutputDirectory / "Artifacts");
+            Directory.CreateDirectory(OutputDirectory / "Artifacts");
+
+            Logger.Info("Copy plugins to artifacts folder");
+            var directories = GlobDirectories(SourceDirectory / "Plugins", $"**/bin/{Configuration}");
+            foreach (var directory in directories)
+            {
+                var di = new DirectoryInfo(directory);
+                var pluginName = di.Parent?.Parent?.Name;
+
+                Directory.CreateDirectory(OutputDirectory / "Artifacts" / "Plugins" / pluginName);
+
+                var files = di.GetFiles("*.dll");
+                foreach (var file in files)
+                {
+                    string outFile = string.Empty;
+
+                    if (file.Name.StartsWith(pluginName))
+                    {
+                        outFile = OutputDirectory / "Artifacts" / "Plugins" / pluginName / $"{Path.GetFileNameWithoutExtension(file.Name)}_plugin.dll";
+                    }
+                    else
+                    {
+                        outFile = OutputDirectory / "Artifacts" / "Plugins" / pluginName / file.Name;
+                    }
+
+                    if (file.Name.StartsWith("aimp_dotnet"))
+                    {
+                        outFile = OutputDirectory / "Artifacts" / "Plugins" / pluginName / $"{pluginName}.dll";
+                    }
+
+                    file.CopyTo(outFile, true);
+                }
+            }
+
+            Logger.Info("Copy SDK files to artifacts folder");
+            var sdkFolder = new DirectoryInfo(SourceDirectory / $"{Configuration}");
+            Directory.CreateDirectory(OutputDirectory / "Artifacts" / "SDK");
+            var sdkFiles = sdkFolder.GetFiles("*.dll");
+            foreach (var file in sdkFiles)
+            {
+                var outFile = OutputDirectory / "Artifacts" / "SDK" / file.Name;
+                file.CopyTo(outFile, true);
+            }
         });
 }
