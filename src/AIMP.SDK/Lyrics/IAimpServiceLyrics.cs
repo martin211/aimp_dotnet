16f20dd664235ea9bac08e12a4a0316504360280
Evgeniy <mail4evgeniy@gmail.com>
2019-02-21 00:14:24 +0300

    Lyrics service (#20)

---
+++ src/AIMP.SDK/Lyrics/IAimpServiceLyrics.cs
@@ -0,0 +1,50 @@
+﻿// ******************************************/
+// Evgeniy Bogdan (c) 2019
+// IAimpServiceLyrics.cs
+// ******************************************/
+
+using System;
+using AIMP.SDK.FileManager;
+
+namespace AIMP.SDK.Lyrics
+{
+    public enum LyricsFlags
+    {
+        /// <summary>
+        /// Search without cache.
+        /// </summary>
+        Nocache = 1,
+
+        /// <summary>
+        /// The method will not return a control until the operation is completed.
+        /// </summary>
+        WaitFor = 4
+    }
+
+    public delegate void AimpServiceLyricsReceive(IAimpLyrics lyrics, object userData);
+
+    /// <summary>
+    /// Allowed access to the song text.
+    /// </summary>
+    public interface IAimpServiceLyrics
+    {
+        /// <summary>
+        /// Gets a lyrics for file.
+        /// </summary>
+        /// <param name="fileInfo"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        AimpActionResult Get(IAimpFileInfo fileInfo, LyricsFlags flags, object userData, out IntPtr taskId);
+
+        /// <summary>
+        /// Aborts a current active task.
+        /// </summary>
+        /// <returns></returns>
+        AimpActionResult Cancel(IntPtr taskId, LyricsFlags flags);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        event AimpServiceLyricsReceive LyricsReceive;
+    }
+}
