7e553bd712e823ef62e847514d6f66e82a5c81f6
martin211 <mail4evgeniy@gmail.com>
2022-01-18 00:52:28 +0300

    Fix build

--- build/Build.cs
+++ build/Build.cs
@@ -25,7 +25,7 @@ using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 [CheckBuildProjectConfigurations]
 partial class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.PrintBuildParameters);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
